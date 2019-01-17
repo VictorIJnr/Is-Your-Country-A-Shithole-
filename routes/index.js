e138012272811824fd9f77c1f516dc711e97db41
Jack Leslie <jjdl@st-andrews.ac.uk>
2019-01-17 23:00:44 +0000

    Upside down american flag, added link to article to make us look less like assholes, made Russia result less PC

--- routes/index.js
+++ routes/index.js
@@ -1,98 +1,109 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var shithole = require("../shithole");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   console.log(req.query.country + "\tHELLO");
-    if (req.query.country) {
+  if (req.query.country) {
+    if (
+      req.query.country.toLowerCase() === "united states of america" ||
+      req.query.country.toLowerCase() === "america"
+    ) {
+      var result = {
+        header:
+          "No, Donald Trump doesn't think " +
+          req.query.country +
+          " is a shithole.",
+        body: "America is Great Again"
+      };
+      res.render("pages/result", {
+        title: "Is Your Country A Shithole?",
+        result: result
+      });
+    }
 
-      if (req.query.country.toLowerCase() === "united states of america" || req.query.country.toLowerCase() === "america") {
-        var result = {
-          header: "No, Donald Trump doesn't think " + req.query.country + " is a shithole.",
-          body: 'America is Great Again'
-        }
-        res.render('pages/result', {
-          title: 'Is Your Country A Shithole?',
-          result: result
-        });
-      }
+    var result = {};
+    // shithole.allShitholes(function(data) {
+    //   console.log(data);
+    //   console.log("Hi");
+    // });
 
+    shithole.isShithole(req.query.country, function(data) {
       var result = {};
-      // shithole.allShitholes(function(data) {
-      //   console.log(data);
-      //   console.log("Hi");
-      // });
 
-      shithole.isShithole(req.query.country, function(data) {
+      if (req.query.country.toLowerCase() === "russia") {
+        result.header = "[Error, this page has been hacked]";
+        result.bodyApprehended = "";
+        result.tagApprehended = "";
 
-        var result = {};
+        result.bodyImmigration = "";
+        result.tagImmigration = "";
 
-        if (req.query.country.toLowerCase() === "russia") {
-
-          result.header = "да, I MEAN YES RUSSIA IS BEST COUNTRY";
-          result.bodyApprehended = "Number of people apprehended 2007-2016: " + 0;
-          result.tagApprehended = "Low";
-
-          result.bodyImmigration = "Percentage of people who love Putin: " + 100 + "%";
-          result.tagImmigration = "High";
-
-          result.bodyGdp = "Country's Average GDP per captia 2007-2016: " + 1000000000000 + " USD";
-          result.tagGdp = "High";
-
-          result.body = "";
-
-          res.render("pages/result", {
-            title: 'Is Your Country A Shithole?',
-            result: result
-          });
-        }
-
-        result.decision = data.isShithole ? "Yes" : "No";
-
-        if (result.decision === "Yes") {
-          result.header = "Yes, Donald Trump thinks " + req.query.country + " is a \"shithole country\"!";
-        } else {
-          result.header = "No, Donald Trump doesn't think " + req.query.country + " is a \"shithole country\".";
-        }
-
-        if (req.query.country.toLowerCase() === "china") {
-          result.header = "No, Donald Trump loves " + req.query.country + "!";
-        }
-
-        result.bodyApprehended = "Number of people apprehended 2007-2016: " + data.apprehended;
-        result.tagApprehended = data.apprehendedTag;
-
-        result.bodyImmigration = "Number of people who entered the country 2007-2016: " + data.total;
-        result.tagImmigration = data.totalTag;
-
-        result.bodyGdp = "Country's Average GDP per captia 2007-2016: " + data.gdp + " USD";
-        result.tagGdp = data.gdpTag;
+        result.bodyGdp = "";
+        result.tagGdp = "";
 
         result.body = "";
 
         res.render("pages/result", {
-          title: 'Is Your Country A Shithole?',
+          title: "Is Your Country A Shithole?",
           result: result
         });
+      }
+
+      result.decision = data.isShithole ? "Yes" : "No";
+
+      if (result.decision === "Yes") {
+        result.header =
+          "Yes, Donald Trump thinks " +
+          req.query.country +
+          ' is a "shithole country"!';
+      } else {
+        result.header =
+          "No, Donald Trump doesn't think " +
+          req.query.country +
+          ' is a "shithole country".';
+      }
+
+      if (req.query.country.toLowerCase() === "china") {
+        result.header = "No, Donald Trump loves " + req.query.country + "!";
+      }
+
+      result.bodyApprehended =
+        "Number of people apprehended 2007-2016: " + data.apprehended;
+      result.tagApprehended = data.apprehendedTag;
+
+      result.bodyImmigration =
+        "Number of people who entered the country 2007-2016: " + data.total;
+      result.tagImmigration = data.totalTag;
+
+      result.bodyGdp =
+        "Country's Average GDP per captia 2007-2016: " + data.gdp + " USD";
+      result.tagGdp = data.gdpTag;
+
+      result.body = "";
+
+      res.render("pages/result", {
+        title: "Is Your Country A Shithole?",
+        result: result
       });
-    } else {
-      res.render('pages/index', {
-        title: 'Is Your Country A Shithole?'
-      });
-    }
+    });
+  } else {
+    res.render("pages/index", {
+      title: "Is Your Country A Shithole?"
+    });
+  }
 });
 
-router.use('/heatmap', function(req, res, next) {
-
+router.use("/heatmap", function(req, res, next) {
   var c = "Australia";
 
-  res.render('pages/heatmap', {
-    title: 'Is Your Country A Shithole?',
+  res.render("pages/heatmap", {
+    title: "Is Your Country A Shithole?",
     gb: shithole.isShithole(c, function(data) {
       console.log(c + " " + data.eval);
       return data.eval;
-    }),
+    })
   });
 });
 
